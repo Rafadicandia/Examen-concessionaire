552220d9e023ca38df5a41badbf1747c9b58e177
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-19 09:22:50 +0100

    builds setter, getters and Gate keepers

--- Concessionaire-App/src/main/java/Car.java
+++ Concessionaire-App/src/main/java/Car.java
@@ -5,9 +5,57 @@ public class Car {
     private int fabricationYear;
 
     public Car(String brand, String model, String fuel, int fabricationYear) {
+
+        if (fabricationYear <= 0) {
+            throw new RuntimeException("Year can't be less than 0 or 0");
+        }
+        if (brand == null || brand.trim().isEmpty()) {
+            throw new RuntimeException("Brand can't be null nor empty");
+        }
+
+        if (model == null || model.trim().isEmpty()) {
+            throw new RuntimeException("Model can't be null nor empty");
+        }
+
+        if (fuel == null || fuel.trim().isEmpty()) {
+            throw new RuntimeException("Fuel can't be null nor empty");
+        }
+
         this.brand = brand;
         this.model = model;
         this.fuel = fuel;
+        this.fabricationYear = fabricationYear;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getFabricationYear() {
+        return fabricationYear;
+    }
+
+    public void setFabricationYear(int fabricationYear) {
         this.fabricationYear = fabricationYear;
     }
 }
