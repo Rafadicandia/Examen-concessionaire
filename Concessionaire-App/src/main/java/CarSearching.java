254eaa59cc2305feef7f1bfd506d2da938d94f27
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-19 09:56:21 +0100

    build filter per year

--- Concessionaire-App/src/main/java/CarSearching.java
+++ Concessionaire-App/src/main/java/CarSearching.java
@@ -13,7 +13,23 @@ public class CarSearching {
         for (Car c : listofCars) {
             System.out.println(c.getCarDescription());
         }
+    }
+
+    public void getCarsAfterYear(int year) {
+
+        List<Car> cars = new ArrayList<>();
+        for (Car c : listofCars) {
+            if (c.getFabricationYear()>year) {
+                cars.add(c);
+            }
+        }
+        System.out.println("<<--List Cars builded after "+year+"-->>");
+        for (Car c : cars) {
+                System.out.println("Brand: "+c.getBrand()+". Model: "+c.getModel()+ "Year: "+c.getFabricationYear());
+        }
 
 
     }
+
+
 }
