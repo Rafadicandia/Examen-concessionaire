70a037d79489b530e3f8531c0d7338d9eb7a276b
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-19 09:20:26 +0100

    core init

---
+++ Concessionaire-App/src/main/java/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    private String brand;
+    private String model;
+    private String fuel;
+    private int fabricationYear;
+
+    public Car(String brand, String model, String fuel, int fabricationYear) {
+        this.brand = brand;
+        this.model = model;
+        this.fuel = fuel;
+        this.fabricationYear = fabricationYear;
+    }
+}
